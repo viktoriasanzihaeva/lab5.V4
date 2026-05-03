904f3304ce29b111af5b20610b81aa17c0620ed9
viktoriasanzihaeva <viktoriasanzihaeva@gmail.com>
2026-05-03 22:08:30 +0800

    Update vowels_count.py

--- vowels_count.py
+++ vowels_count.py
@@ -1,7 +1,7 @@
-```python
-# vowels_count.py
-
 def count_vowels(text: str) -> int:
+    """
+    Возвращает количество гласных букв (a, e, i, o, u) в строке text.
+    Регистр не учитывается.
+    """
     vowels = set('aeiou')
     return sum(1 for char in text.lower() if char in vowels)
-```
